156ad04265f09b3e6279dbd3ba63f1a9113b8e5d
Luiza Graciano <luizagraciano.m@gmail.com>
2024-04-16 22:34:50 -0300

    ajustes

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,19 +6,21 @@ n2 = int(input("Digite o segundo número: "))
 numero_encontrado = False
 
 #Busca binária
-while numero_encontrado == False:
+while not numero_encontrado:
 
     #Para dividir ao meio as possibilidades
     verificador_numero = int((n1 + n2) / 2)
 
     #Confirmação com o usuário
-    maior_que = input(f"O número que você pensou é maior que {verificador_numero}? Digite SIM ou NÃO: ")
+    maior_que = input(f"O número que você pensou é maior que {verificador_numero}? Digite SIM ou NÃO: ").upper()
 
+    #Validação da resposta do usuário, reajusta o valor mínimo ou máximo
     if maior_que == "SIM":
         n1 = verificador_numero + 1
     else:
         n2 = verificador_numero
 
+    #Condição de adivinhação do número
     if n1 == n2:
         numero_encontrado = True
         print(f"O seu número é {n1}!")
